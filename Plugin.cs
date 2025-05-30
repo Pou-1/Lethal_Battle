d392641e2bf01e36b80f70ed1c2093c848820177
Emily <cypr.jeux@gmail.com>
2025-05-30 18:25:39 +0200

    Lethal Battle start

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace Lethal_Battle
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        const string GUID = "POUY.LETHAL_BATTLE";
+        const string NAME = "Lethal Battle";
+        const string VERSION = "0.0.1";
+        static Plugin instance;
+        static ManualLogSource log;
+
+        public void Awake()
+        {
+            instance = this;
+            log = Logger;
+            log.LogMessage("Lethal Battle Loaded !");
+        }
+    }
+}
