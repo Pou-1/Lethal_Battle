2b0848151a3a2770ebe17ddd4caa1e163a7d2101
Emily <cypr.jeux@gmail.com>
2025-05-31 21:08:53 +0200

    start of listing banned items from spawning in the battle

---
+++ ManageBattle.cs
@@ -0,0 +1,140 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+using LethalLib.Modules;
+using System.Collections.Generic;
+using PremiumScraps.Utils;
+using System.Linq;
+
+namespace Lethal_Battle
+{
+    internal class ManageBattle
+    {
+        public static void ItemsSpawner()
+        {
+            if (GameNetworkManager.Instance.localPlayerController.IsHost == false)
+            {
+                return;
+            }
+
+            List<Item> scraps = new List<Item>();
+            string[] bannedScraps = { 
+                "Boombox",
+                "Flashlight",
+                "Key",
+                "LockPicker",
+                "LungApparatus",
+                "MapDevice",
+                "ProFlashlight",
+                "WalkieTalkie",
+                "7Ball",
+                "Bell",
+                "BigBolt",
+                "BottleBin",
+                "Brush",
+                "CashRegister",
+                "ChemicalJug",
+                "ClownHorn",
+                "Cog1",
+                "Dentures",
+                "DustPan",
+                "EggBeater",
+                "EnginePart1",
+                "FancyCup",
+                "FancyLamp",
+                "FishTestProp",
+                "FlashLaserPointer",
+                "GoldBar",
+                "MagnifyingGlass",
+                "MetalSheet",
+                "MoldPan",
+                "Mug",
+                "PerfumeBottle",
+                "Phone",
+                "PickleJar",
+                "PillBottle",
+                "Ring",
+                "RobotToy",
+                "RubberDuck",
+                "SodaCanRed",
+                "SteeringWheel",
+                "TeaKettle",
+                "ToyCube",
+                "RedLocustHive",
+                "RadarBooster",
+                "GunAmmo",
+                "SprayPaint",
+                "GiftBox",
+                "WhoopieCushion",
+                "WeedKillerBottle",
+                "SoccerBall",
+                "ControlPad",
+                "GarbageLid",
+                "PlasticCup",
+                "ToiletPaperRolls",
+                "Clock",
+                "ToyTrain",
+                "BeltBag",
+                "Zeddog",
+
+                "FrierenItem",
+                "ChocoboItem",
+                "AinzOoalGownItem",
+                "HelmDominationItem",
+                "TheKingItem",
+                "HarryMasonItem",
+                "CristalItem",
+                "PuppySharkItem",
+                "RupeeItem",
+                "EaNasirItem",
+                "SODAItem",
+                "CroutonItem",
+                "BalanItem",
+                "MoogleItem",
+                "GazpachoItem",
+                "AbiItem",
+            };
+
+            /*
+            foreach (var item in Items.scrapItems)
+                scraps.Add(item.item);
+            foreach (var item in Items.shopItems)
+                scraps.Add(item.item);
+            foreach (var item in Items.plainItems)
+                scraps.Add(item.item);
+            */
+            foreach (var item in StartOfRound.Instance.allItemsList.itemsList)
+                scraps.Add(item);
+
+            scraps.RemoveAll(item => (bannedScraps.Contains(item.itemName)));
+            //scraps.RemoveAll(item => (item == null || item.spawnPrefab == null || item.spawnPrefab.GetComponent<Scrap>() != null));
+
+            Vector3 spawnPosition = new Vector3(24, -11, 12);
+
+            for (int i = 0; i < scraps.Count; i++)
+            {
+                Plugin.log.LogError(scraps[i].name);
+                //SpawnScrap(scraps[i], spawnPosition);
+            }
+
+            /*SpawnableMapObject scrapToSpawn = new SpawnableMapObject();
+            Vector3 spawnPosition = new Vector3(24, -11, 12);
+            for (int i = 0; i<StartOfRound.Instance.allPlayerObjects.Length; i++)
+            {
+                SpawnScrap(itemNameToSpawn, spawnPosition);
+            }*/
+        }
+
+        private static void SpawnScrap(Item scrap, Vector3 position)
+        {
+            GameObject gameObject = Object.Instantiate(scrap.spawnPrefab, position,
+                Quaternion.identity, RoundManager.Instance.spawnedScrapContainer.transform);
+
+            GrabbableObject component = gameObject.GetComponent<GrabbableObject>();
+            component.transform.rotation = Quaternion.Euler(component.itemProperties.restingRotation);
+            component.fallTime = 0f;
+            component.scrapValue = 0;
+            component.NetworkObject.Spawn();
+            component.FallToGround(true);
+        }
+    }
+}
--- Patch.cs
+++ Patch.cs
@@ -11,16 +11,17 @@ namespace Lethal_Battle
         [HarmonyPostfix]
         [HarmonyPatch("FinishGeneratingNewLevelClientRpc")]
 
-        public static void Changes() 
+        public static void Changes() // is called on the start of a new level
         {
-            if(TimeOfDay.Instance.daysUntilDeadline == 0 && TimeOfDay.Instance.currentLevel.PlanetName == "71 Gordion")
+            ManageBattle.ItemsSpawner();
+            if (TimeOfDay.Instance.daysUntilDeadline == 0 && TimeOfDay.Instance.currentLevel.PlanetName == "71 Gordion")
             {
                 Plugin.log.LogError("In gordion for the last phase!");
-                int potentialBodiesValue = 5 * (StartOfRound.Instance.allPlayerObjects.Length - 1);
+                int potentialBodiesValue = 5 * (StartOfRound.Instance.allPlayerObjects.Length - 1); // Cout value of every player except one * 5 
                 int scrapsValue = Object.FindObjectsOfType<GrabbableObject>().Where(o => o.itemProperties.isScrap && o.itemProperties.minValue > 0
                     && (!(o is StunGrenadeItem g) || !g.hasExploded || !g.DestroyGrenade)
                     && (o.isInShipRoom == true && o.isInElevator == true)).ToList().Sum(s => s.scrapValue);
-                if (scrapsValue + potentialBodiesValue + TimeOfDay.Instance.quotaFulfilled >= TimeOfDay.Instance.profitQuota)
+                if (scrapsValue + potentialBodiesValue + TimeOfDay.Instance.quotaFulfilled >= TimeOfDay.Instance.profitQuota) // quota not reached and day is quota day
                 {
                     Plugin.log.LogError("No battle !");
                 }
@@ -28,7 +29,6 @@ namespace Lethal_Battle
                 {
                     Plugin.log.LogError("battle !");
                 }
-
             }
         }
     }
