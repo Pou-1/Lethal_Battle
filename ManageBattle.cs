98e726557857d19d5c66db6bcc89ef1def72240d
Emily <cypr.jeux@gmail.com>
2025-06-04 01:30:36 +0200

    banned scraps

--- ManageBattle.cs
+++ ManageBattle.cs
@@ -1,9 +1,8 @@
-﻿using Unity.Netcode;
-using UnityEngine;
-using LethalLib.Modules;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using PremiumScraps.Utils;
 using System.Linq;
+using System;
+using Unity.Netcode;
 
 namespace Lethal_Battle
 {
@@ -17,123 +16,47 @@ namespace Lethal_Battle
             }
 
             List<Item> scraps = new List<Item>();
-            string[] bannedScraps = { 
-                "Boombox",
-                "Flashlight",
-                "Key",
-                "LockPicker",
-                "LungApparatus",
-                "MapDevice",
-                "ProFlashlight",
-                "WalkieTalkie",
-                "7Ball",
-                "Bell",
-                "BigBolt",
-                "BottleBin",
-                "Brush",
-                "CashRegister",
-                "ChemicalJug",
-                "ClownHorn",
-                "Cog1",
-                "Dentures",
-                "DustPan",
-                "EggBeater",
-                "EnginePart1",
-                "FancyCup",
-                "FancyLamp",
-                "FishTestProp",
-                "FlashLaserPointer",
-                "GoldBar",
-                "MagnifyingGlass",
-                "MetalSheet",
-                "MoldPan",
-                "Mug",
-                "PerfumeBottle",
-                "Phone",
-                "PickleJar",
-                "PillBottle",
-                "Ring",
-                "RobotToy",
-                "RubberDuck",
-                "SodaCanRed",
-                "SteeringWheel",
-                "TeaKettle",
-                "ToyCube",
-                "RedLocustHive",
-                "RadarBooster",
-                "GunAmmo",
-                "SprayPaint",
-                "GiftBox",
-                "WhoopieCushion",
-                "WeedKillerBottle",
-                "SoccerBall",
-                "ControlPad",
-                "GarbageLid",
-                "PlasticCup",
-                "ToiletPaperRolls",
-                "Clock",
-                "ToyTrain",
-                "BeltBag",
-                "Zeddog",
 
-                "FrierenItem",
-                "ChocoboItem",
-                "AinzOoalGownItem",
-                "HelmDominationItem",
-                "TheKingItem",
-                "HarryMasonItem",
-                "CristalItem",
-                "PuppySharkItem",
-                "RupeeItem",
-                "EaNasirItem",
-                "SODAItem",
-                "CroutonItem",
-                "BalanItem",
-                "MoogleItem",
-                "GazpachoItem",
-                "AbiItem",
-            };
-
-            /*
-            foreach (var item in Items.scrapItems)
-                scraps.Add(item.item);
-            foreach (var item in Items.shopItems)
-                scraps.Add(item.item);
-            foreach (var item in Items.plainItems)
-                scraps.Add(item.item);
-            */
             foreach (var item in StartOfRound.Instance.allItemsList.itemsList)
                 scraps.Add(item);
 
-            scraps.RemoveAll(item => (bannedScraps.Contains(item.itemName)));
-            //scraps.RemoveAll(item => (item == null || item.spawnPrefab == null || item.spawnPrefab.GetComponent<Scrap>() != null));
+            string[] banScraps = BanScraps.getBannedScraps();
+
+            scraps.RemoveAll(item =>
+                banScraps.Any(banned =>
+                    string.Equals(banned.Trim(), item.itemName.Trim().ToUpper(), StringComparison.OrdinalIgnoreCase)
+                )
+            );
 
             Vector3 spawnPosition = new Vector3(24, -11, 12);
 
+            Plugin.log.LogError("scraps======================");
             for (int i = 0; i < scraps.Count; i++)
             {
                 Plugin.log.LogError(scraps[i].name);
                 //SpawnScrap(scraps[i], spawnPosition);
             }
 
-            /*SpawnableMapObject scrapToSpawn = new SpawnableMapObject();
-            Vector3 spawnPosition = new Vector3(24, -11, 12);
-            for (int i = 0; i<StartOfRound.Instance.allPlayerObjects.Length; i++)
-            {
-                SpawnScrap(itemNameToSpawn, spawnPosition);
-            }*/
+            Plugin.log.LogError("SPAWN==========================");
+            Plugin.log.LogError(scraps[1].itemName);
+            SpawnScrap(scraps[1], spawnPosition);
+
+            //StartOfRound.Instance.allPlayerObjects.Length;
         }
 
         private static void SpawnScrap(Item scrap, Vector3 position)
         {
-            GameObject gameObject = Object.Instantiate(scrap.spawnPrefab, position,
+            GameObject gameObject = UnityEngine.Object.Instantiate(scrap.spawnPrefab, position,
                 Quaternion.identity, RoundManager.Instance.spawnedScrapContainer.transform);
 
             GrabbableObject component = gameObject.GetComponent<GrabbableObject>();
             component.transform.rotation = Quaternion.Euler(component.itemProperties.restingRotation);
             component.fallTime = 0f;
             component.scrapValue = 0;
-            component.NetworkObject.Spawn();
+            component.FallToGround(true);
+            NetworkObject netObj = gameObject.GetComponent<NetworkObject>();
+            netObj.Spawn();
+            //component.NetworkObject.Spawn();
             component.FallToGround(true);
         }
     }
